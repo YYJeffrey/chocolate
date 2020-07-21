59cecf6cbc7be2bb1cb5f7964153d71d9c684af2
Jeffrey <jeffrey_ye@qq.com>
2020-07-21 19:15:49 +0800

    feat: 详细列表

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -3,15 +3,41 @@
     <a-card class="card">
       <a-card-grid v-for="item in stockBoard" :key="item.code" class="card-grid">
         <span class="card-title">{{ item.name }}</span>
-        <span :class="'index ' + (item.price - item.close >= 0 ? 'red' : 'green')">{{ item.price }}</span>
+        <span :class="'index ' + (item.priceChange >= 0 ? 'red' : 'green')">{{ item.price }}</span>
         <div class="increase-box">
-          <span :class="'increase-number ' + (item.price - item.close >= 0 ? 'red' : 'green')">
-            {{ ((item.price - item.close >= 0 ? '+' : '-') + item.price - item.close) | formatAmount }}
-          </span>
-          <span :class="'increase-percent ' + (item.price - item.close >= 0 ? 'red' : 'green')">{{ (item.changePercent >= 0 ? '+' : '-') + item.changePercent + '%' }}</span>
+          <span :class="'increase-number ' + (item.priceChange >= 0 ? 'red' : 'green')">{{ (item.priceChange > 0 ? '+' : '') + item.priceChange }}</span>
+          <span :class="'increase-percent ' + (item.priceChange >= 0 ? 'red' : 'green')">{{ (item.changePercent > 0 ? '+' : '') + item.changePercent + '%' }}</span>
         </div>
       </a-card-grid>
     </a-card>
+
+    <a-table class="table" :data-source="listData" :pagination="false" size="small">
+      <a-table-column title="名称" data-index="name" align="center">
+        <template slot-scope="name">
+          <span class="column">{{ name }}</span>
+        </template>
+      </a-table-column>
+      <a-table-column title="代码" data-index="code" align="center">
+        <template slot-scope="code">
+          <span class="column">{{ code }}</span>
+        </template>
+      </a-table-column>
+      <a-table-column title="价格" data-index="price" align="center">
+        <template slot-scope="price">
+          <span class="column">{{ price }}</span>
+        </template>
+      </a-table-column>
+      <a-table-column title="涨幅" data-index="percent" align="center">
+        <template slot-scope="percent">
+          <span class="column">{{ percent }}</span>
+        </template>
+      </a-table-column>
+      <a-table-column title="时间" data-index="date" align="center">
+        <template slot-scope="date">
+          <span class="column">{{ date }}</span>
+        </template>
+      </a-table-column>
+    </a-table>
   </div>
 </template>
 
@@ -23,6 +49,24 @@ export default {
   data() {
     return {
       stockBoard: [],
+      listData: [
+        {
+          name: '上证指数',
+          code: 'sh000001',
+          price: '3330.57',
+          percent: '0.50',
+          date: '07-20 09:35',
+          type: 'stock',
+        },
+        {
+          name: '天弘沪深300ETF联接A',
+          code: '000961',
+          price: 1.4608,
+          percent: '2.8517',
+          date: '07-20 09:35',
+          type: 'fund',
+        },
+      ],
     };
   },
   mounted() {
@@ -31,13 +75,6 @@ export default {
     if (util.isDealingTime()) {
       setInterval(this.getStockBoard, 3000);
     }
-  },
-  filters: {
-    formatAmount(value) {
-      let tempVal = parseFloat(value).toFixed(3);
-      let realVal = tempVal.substring(0, tempVal.length - 1);
-      return realVal;
-    },
   },
   methods: {
     /**
@@ -57,8 +94,6 @@ export default {
 
 <style lang="scss" scoped>
 .content {
-  width: auto;
-
   .card {
     width: 541px;
 
@@ -75,6 +110,7 @@ export default {
 
       .card-title {
         color: #141414;
+        font-weight: bold;
       }
 
       .index {
@@ -88,6 +124,18 @@ export default {
         }
       }
     }
+  }
+
+  .table {
+    margin-top: 38px;
+
+    .column {
+      font-size: 12px;
+    }
+  }
+
+  .ant-table-body {
+    margin: 0 !important;
   }
 
   .red {
