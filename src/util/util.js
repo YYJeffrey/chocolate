53863abcb951d095fb27204416b504e3c94a7fa1
YYJeffrey <501934027@qq.com>
2020-08-06 23:37:59 +0800

    fix: 基金净值休市计算

--- src/util/util.js
+++ src/util/util.js
@@ -1,42 +1,49 @@
 /**
- * 是否为交易时间
+ * 是否为交易日
  */
-function isDealingTime() {
+function isDealingDay() {
   const date = new Date();
   const day = date.getDay();
 
   if (day !== 0 && day !== 6) {
-    const startAMTime = new Date(new Date().setHours(9, 30, 0, 0));
-    const endAMTime = new Date(new Date().setHours(11, 30, 0, 0));
-    const startPMTime = new Date(new Date().setHours(13, 0, 0, 0));
-    const endPMTime = new Date(new Date().setHours(15, 0, 0, 0));
-
-    if ((date >= startAMTime && date <= endAMTime) || (date >= startPMTime && date <= endPMTime)) {
-      return true;
-    }
+    return true;
   }
   return false;
 }
 
 /**
- * 是否为交易日午休时间
+ * 是否为交易时间
  */
-function isNoonBreakTime() {
+function isDealingTime() {
   const date = new Date();
-  const day = date.getDay();
 
-  if (day !== 0 && day !== 6) {
+  if (isDealingDay()) {
+    const startAMTime = new Date(new Date().setHours(9, 30, 0, 0));
     const endAMTime = new Date(new Date().setHours(11, 30, 0, 0));
     const startPMTime = new Date(new Date().setHours(13, 0, 0, 0));
-
-    if (date >= endAMTime && date <= startPMTime) {
+    const endPMTime = new Date(new Date().setHours(15, 0, 0, 0));
+    if ((date >= startAMTime && date <= endAMTime) || (date >= startPMTime && date <= endPMTime)) {
       return true;
     }
+  }
+  return false
+}
+
+/**
+ * 是否是今天
+ * @param {日期字符串} str 
+ */
+function isToday(str) {
+  const d = new Date(str.replace(/-/g, "/"));
+  const todaysDate = new Date();
+  if (d.setHours(0, 0, 0, 0) == todaysDate.setHours(0, 0, 0, 0)) {
+    return true;
   }
   return false;
 }
 
 export default {
+  isDealingDay,
   isDealingTime,
-  isNoonBreakTime,
+  isToday
 };
